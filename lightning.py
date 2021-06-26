12d55635d8657b4d632f7914ff3f7c0dd33437f9
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 00:59:19 -0400

    Add imports
    (Note that this script is generated from a Jupyter notebook which has not been committed yet)

---
+++ lightning.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[67]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+import numpy as np
+import numba as nb
