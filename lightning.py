0646ea0c0afbb8821ae315fc42dec1f9d27853c6
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-06-26 01:11:48 -0400

    Display vector field with Matplotlib

--- lightning.py
+++ lightning.py
@@ -27,3 +27,21 @@ x, y = np.meshgrid(*[np.linspace(0, 1, R)]*2)
 
 u = np.sin(np.pi * x) * np.cos(np.pi * y)+np.random.normal(0, 0.1, [R, R])
 v = -np.cos(np.pi * x) * np.sin(np.pi * y)+np.random.normal(0, 0.1, [R, R])
+
+fig = plt.figure(figsize=(10,)*2)
+plt.rcParams['figure.figsize'] = [10, 10]
+fig, ax = plt.subplots()
+
+# Refer to https://stackoverflow.com/a/40026959/10940584
+norm = matplotlib.colors.Normalize()
+colors = np.arctan2(u, v)
+norm.autoscale(colors)
+cmap = matplotlib.cm.inferno(norm(colors))
+
+def Norm(a, b):
+    return (a ** 2 + b ** 2) ** 0.5
+
+theta = np.angle(np.apply_along_axis(lambda a: complex(*a), 0, np.stack([u, v])))
+ax.quiver(x, y, u, v, theta, cmap='hsv')
+plt.axis('off')
+plt.show()
